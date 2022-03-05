1d85f069237b32a1b0a61089dbd554de77c5fc11
Sze Ying <ting.szeying@gmail.com>
2022-03-05 15:52:10 +0800

    fix: differentiate remove command

--- src/models/bot/bot.ts
+++ src/models/bot/bot.ts
@@ -1,6 +1,13 @@
 import { Telegraf, Context } from "telegraf";
 import { remove, vote } from "./actions";
-import { addPoll, chatId, hi, listPolls, start } from "./commands";
+import {
+  addPoll,
+  chatId,
+  hi,
+  listPolls,
+  start,
+  remove as removeCommand,
+} from "./commands";
 
 const setCommands = (bot: Telegraf<Context>) => {
   bot
@@ -8,7 +15,7 @@ const setCommands = (bot: Telegraf<Context>) => {
     .command("chatid", chatId)
     .command("hi", hi)
     .command("list", listPolls)
-    .command("remove", remove)
+    .command("remove", removeCommand)
     .command("start", start)
     .action(/VOTE_[0-9]+/, vote)
     .action(/REMOVE_[A-Za-z0-9]+/, remove);
--- src/models/bot/commands/index.ts
+++ src/models/bot/commands/index.ts
@@ -2,4 +2,5 @@ export * from "./addPoll";
 export * from "./chatId";
 export * from "./hi";
 export * from "./listPolls";
+export * from "./remove";
 export * from "./start";
